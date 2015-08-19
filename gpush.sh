02d84a9b1d306f4597c0fbcff9d9f5c79608aba9
obenchekroun <othmane@me.com>
2015-08-19 10:26:07 +0200

    Updated packages

--- gpush.sh
+++ gpush.sh
@@ -2,5 +2,5 @@
 git add --all
 git commit -m "$*"
 git push
-terminal-notifier -title "Github repo updated !" -subtitle "Repo ready to use !" -message "Changes to the repo committed and pushed online"
+terminal-notifier -title "Github repo updated !" -subtitle "Repo ready to use !" -message "Changes committed and pushed online"
 
